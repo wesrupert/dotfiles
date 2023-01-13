aacd33db9512c6b1af45425b7dacf4558c4961f4
Wes Rupert <rupertwe@amazon.com>
2023-01-13 14:17:50 -0800

    chore: update package list

--- install.ps1
+++ install.ps1
@@ -9,15 +9,15 @@ param(
 
 function Install-Dir {
   param ([string]$dir, [string]$path)
-    echo "Installing $dir to $path..."
-    Get-ChildItem "$PSScriptRoot\$dir" | Foreach-Object {
-      $orig = $_.FullName
-        $file = $_.Name
-        $dest = "$path\$file"
-        if (-not (Test-Path -Path $dest)) {
-          New-Item -ItemType SymbolicLink -Target $orig -Path $dest -ErrorAction Stop
-        }
+  echo "Installing $dir to $path..."
+  Get-ChildItem "$PSScriptRoot\$dir" | Foreach-Object {
+    $orig = $_.FullName
+    $file = $_.Name
+    $dest = "$path\$file"
+    if (-not (Test-Path -Path $dest)) {
+      New-Item -ItemType SymbolicLink -Target $orig -Path $dest -ErrorAction Stop
     }
+  }
 }
 
 function Install-File {
@@ -36,47 +36,47 @@ function Install-File {
 
 function Install-WingetPackages {
   param ([string[]]$packages)
-    $packages.ForEach({
-      winget list -e --id $_
-      if ($?) {
-        Write-Host "$_ already installed, skipping..."
-      } else {
-        Write-Host "Installing $_..."
-        winget install -e -s winget --accept-package-agreements --accept-source-agreements --id $_
-      }
-    })
+  $packages.ForEach({
+    Write-Host "Installing $_..."
+    winget list -e --id $_
+    if ($?) {
+      Write-Host "$_ already installed, skipping..."
+    } else {
+      Write-Host "$_ not installed, installing $_..."
+      winget install -e -s winget --accept-package-agreements --accept-source-agreements --id $_
+    }
+  })
 }
 
 # Install packages
 if (-not $n) {
   Write-Host 'Installing common packages...'
+  Install-WingetPackages (
+    '7zip.7zip', 'AgileBits.1Password' 'Armin2208.WindowsAutoNightMode',
+    'Git.Git', 'GitHub.cli', 'Google.Chrome', 'JanDeDobbeleer.OhMyPosh',
+    'Lexikos.AutoHotkey', 'Microsoft.PowerShell.Preview',
+    'Microsoft.PowerToys', 'Microsoft.VisualStudioCode',
+    'Microsoft.WindowsTerminal', 'Mozilla.Firefox', 'Neovim.Neovim.Nightly',
+    'Obsidian.Obsidian', 'Python.Python.3.10', 'Spotify.Spotify' 'Yarn.Yarn',
+    'Zoom.Zoom'
+  )
+
+  if ($h -or ((-not $w) -and ($(Read-Host -Prompt 'Install home packages? (y/N)') -eq 'y'))) {
+    Write-Host 'Installing home packages...'
     Install-WingetPackages (
-        '7zip.7zip', 'AgileBits.1Password', 'ApacheFriends.Xampp.8.1',
-        'Armin2208.WindowsAutoNightMode', 'Git.Git', 'GitHub.cli', 'Google.Chrome',
-        'JanDeDobbeleer.OhMyPosh', 'Lexikos.AutoHotkey', 'Logitech.GHUB',
-        'Logitech.OptionsPlus', 'Microsoft.PowerShell.Preview', 'Microsoft.PowerToys',
-        'Microsoft.VisualStudioCode', 'Microsoft.WindowsTerminal', 'Mozilla.Firefox',
-        'Neovim.Neovim.Nightly', 'Obsidian.Obsidian', 'Python.Python.3.10',
-        'Spotify.Spotify', 'SyncTrayzor.SyncTrayzor', 'Yarn.Yarn', 'Zoom.Zoom'
-        )
+      'Discord.Discord', 'Foxit.FoxitReader', 'Mojang.MinecraftLauncher',
+      'OpenWhisperSystems.Signal', 'Samsung.DeX', 'Telegram.TelegramDesktop',
+      'Ultimaker.Cura', 'Valve.Steam', 'VideoLAN.VLC'
+    )
+  }
 
-    if ($h -or ((-not $w) -and ($(Read-Host -Prompt 'Install home packages? (y/N)') -eq 'y'))) {
-      Write-Host 'Installing home packages...'
-        Install-WingetPackages (
-            'Discord.Discord', 'Foxit.FoxitReader', 'Mojang.MinecraftLauncher',
-            'OpenWhisperSystems.Signal', 'Samsung.DeX', 'Telegram.TelegramDesktop',
-            'Ultimaker.Cura', 'Valve.Steam', 'VideoLAN.VLC'
-            )
-    }
-
-# Work
   if ($w -or ((-not $h) -and ($(Read-Host -Prompt 'Install work packages? (y/N)') -eq 'y'))) {
     Write-Host 'Installing work packages...'
-      Install-WingetPackages (
-          'Asana.Asana', 'Figma.Figma', 'GitHub.GitHubDesktop',
-          'Hashicorp.Vagrant', 'Loom.Loom', 'Oracle.VirtualBox',
-          'OpenVPNTechnologies.OpenVPNConnect', 'SlackTechnologies.Slack'
-          )
+    Install-WingetPackages (
+      'Asana.Asana', 'ApacheFriends.Xampp.8.1', 'Figma.Figma'
+      'Hashicorp.Vagrant', 'Loom.Loom', 'Oracle.VirtualBox',
+      'OpenVPNTechnologies.OpenVPNConnect', 'SlackTechnologies.Slack'
+    )
   }
 }
 
@@ -95,9 +95,9 @@ windows\windowsTerminal_themeToggler.ps1
 $firefoxprofiles = "$($env:APPDATA)\Mozilla\Firefox\Profiles"
 Get-ChildItem "$firefoxprofiles" | Foreach-Object {
   $profile = $_.FullName
-    $dest = "$profile\chrome"
-    New-Item -Type Directory $dest -ErrorAction SilentlyContinue
-    Install-Dir -Dir 'firefox' -Path "$dest"
+  $dest = "$profile\chrome"
+  New-Item -Type Directory $dest -ErrorAction SilentlyContinue
+  Install-Dir -Dir 'firefox' -Path "$dest"
 }
 
 echo 'Done!'
