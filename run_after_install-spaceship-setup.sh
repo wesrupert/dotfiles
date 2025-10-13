161b6c45278de4705bf1df91fc3ac605f1e63ec9
Wes Rupert <wrupert@logitech.com>
2025-10-13 13:51:28 -0700

    add zellij

--- run_after_install-spaceship-setup.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-if [[ -n "$ZSH_CUSTOM" && -d "$ZSH_CUSTOM/themes/spaceship-prompt" ]]; then
-  ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
-fi
